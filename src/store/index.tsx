10342ada5c61a3764b5183a29d633954674ee1e3
teseo <sol@javiermellado.com>
2019-02-25 12:41:09 +0000

    fix redux dev tool

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -6,9 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  reducers,
-  initialState,
-  compose(applyMiddleware(...middleware))
-);
+const configureStore = (initialState: Object = {}) => {
+  // @ts-ignore
+  const enableDevTools = !!window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  // @ts-ignore
+  const composeEnhancers = enableDevTools ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+  const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+  return createStore(reducers, initialState, enhancers);
+};
+
+const store = configureStore({});
+
+
 export default store;
